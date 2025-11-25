142b2b015d89e3a6a32560ea7d321fefae5f1608
Sami Awadh <eng.sami.cyber@gmail.com>
2025-11-25 15:36:32 +0200

    create ApplicaationDbContextFactory to fix Add-Migtation problem

---
+++ Feed.DAL/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Feed.Domain.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        builder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=FeedDb;Username=postgres;Password=RonaldoRonaldo_777");
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
