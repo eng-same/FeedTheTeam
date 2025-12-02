cd3e9750af23804d04360f01a706e94ab3ef2c15
Sami Awadh <eng.sami.cyber@gmail.com>
2025-12-02 18:44:30 +0200

    register missing services

--- Feed.Application/ServiceCollectionExtensions.cs
+++ Feed.Application/ServiceCollectionExtensions.cs
@@ -1,6 +1,16 @@
-﻿using Feed.Application.Interfaces;
+﻿using Feed.Application.Commands.Account;
+using Feed.Application.Commands.Pool;
+using Feed.Application.Commands.PoolOptions;
+using Feed.Application.Commands.Vote;
+using Feed.Application.Interfaces;
+using Feed.Application.Requests.Account;
+using Feed.Application.Requests.Pool;
+using Feed.Application.Requests.PoolOption;
+using Feed.Application.Requests.Vote;
 using Feed.Application.Services;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Feed.Application;
 
@@ -11,6 +21,11 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IPoolService, PoolService>();
         services.AddScoped<IVoteService, VoteService>();
         services.AddScoped<IPoolOptionService, PoolOptionService>();
+        services.AddScoped<IValidator<VoteRequest>, VoteRequestValidator>();
+        services.AddScoped<IValidator<CreatePoolRequest>, CreatePoolValidator>();
+        services.AddScoped<IValidator<CreatePoolOptionRequest>, CreatePoolOptionValidator>();
+        services.AddScoped<IValidator<RegisterRequest>, RegisterValidator>();
+        services.AddScoped<IValidator<LoginRequest>, LoginValidator>();
         return services;
     }
 }
